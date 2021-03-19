fe106352155d26fab477b9bb44008dd145a34496
Stefan Malanik <stefanmalanik@gmail.com>
2021-03-19 23:37:09 +0200

    Removed main testing

--- R_Trees/R_Trees.cpp
+++ R_Trees/R_Trees.cpp
@@ -7,35 +7,6 @@
 int main(){
     using namespace std;
 
-    Node n = Node(2, true, 3);
-    Rect a({0, 1}, {2, 3});
-    Rect b({4, 5}, {6, 7});
-    n.childrenBB[0] = a;
-    n.childrenBB[1] = b;
-    n.childrenBB[2] = a;
-    
-    n.shapeDataOffset[0] = Offset(0);
-    n.shapeDataOffset[1] = Offset(1);
-    n.shapeDataOffset[2] = Offset(2);
-
-    auto res = Node::serialize(n);
-    auto nodeRes = Node::deserialize(res, n.degree);
-    
-    for (int i = 0; i < nodeRes->arrSize; i++) {
-        cout << nodeRes->childrenBB[i].getDownLeft().x << ", ";
-        cout << nodeRes->childrenBB[i].getDownLeft().y << " | ";
-        cout << nodeRes->childrenBB[i].getUpRight().x << ", ";
-        cout << nodeRes->childrenBB[i].getUpRight().y << endl;
-    } 
-    cout << endl << "Shp" << endl;
-    for (int i = 0; i < nodeRes->shapeDataOffset.size(); i++) {
-        cout << hex << nodeRes->shapeDataOffset[i].get() << ", ";
-    }   
-    cout << endl << "children" << endl;
-    for (int i = 0; i < nodeRes->childrenOffset.size(); i++) {
-        cout << hex << nodeRes->childrenOffset[i].get() << ", ";
-    }
-    cout << endl;
 
     system("pause");
 
