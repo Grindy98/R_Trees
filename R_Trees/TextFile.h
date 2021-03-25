58b5a422998ebb13d15e8a4d271cec9b08b95cd8
Stefan Malanik <stefanmalanik@gmail.com>
2021-03-26 00:43:20 +0200

    Fixed bugs of TextFile class and ignored text file resource

--- R_Trees/TextFile.h
+++ R_Trees/TextFile.h
@@ -7,13 +7,13 @@ class TextFile : public DataFile
 {
 public:
 	TextFile(string fileName);
-private:
-	fstream textStream;
-	vector<Offset> lineBegins;
-
 	// Inherited via DataFile
 	virtual Entry getEntry(Offset off) override;
 	virtual char getDataFileType() const override;
 	virtual unsigned getNumberOfElements() const override;
+private:
+	fstream textStream;
+	vector<streampos> lineBegins;
+
 };
 
