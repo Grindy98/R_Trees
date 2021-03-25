389dd50d891703447f9de1652260341353f340db
Stefan Malanik <stefanmalanik@gmail.com>
2021-03-26 00:46:20 +0200

    Redone main

--- R_Trees/R_Trees.cpp
+++ R_Trees/R_Trees.cpp
@@ -3,37 +3,21 @@
 #include <random>
 #include <ctime>
 #include "Tree.h"
-#include "TestFile.h"
+#include "TextFile.h"
 
 
 int main() {
     using namespace std;
-    try
-    {
-        auto test = make_unique<TestFile>();
-        Tree t(move(test), "index", 3);
+    TextFile txt = TextFile("locations.txt");
 
-        uniform_real_distribution<double> unif(-10.0, 10.0);
-        default_random_engine re;
-        re.seed(time(0));
-        for (int i = 0; i < 1000; i++) {
-            Point p1, p2;
-            p1 = { unif(re), unif(re) };
-            //p2 = { unif(re), unif(re) };
-            //if (p1.x > p2.x) {
-            //    swap(p1.x, p2.x);
-            //}
-            //if (p1.y > p2.y) {
-            //    swap(p1.y, p2.y);
-            //}
-            t.insert(make_pair(Rect(p1, p1), Offset(-1)));
-        }
-
-        auto res = t.search({ 0.0, 0.0 }, 1);
-        cout << res.size();
-    }
-    catch (const exception& e)
+    for (unsigned i = 0; i < txt.getNumberOfElements(); i++)
     {
-        cout << e.what() << endl;
+        auto entry = txt.getEntry(Offset(i));
+        cout << entry.name << ", " << entry.tag << " | " <<
+            entry.BB.getDownLeft().x << ", " << entry.BB.getDownLeft().y << endl;
     }
+
+    system("pause");
+
+    return 0;
 }
