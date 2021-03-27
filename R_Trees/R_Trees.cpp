2f2d4a7a98d2cbd73177bb352343615594a6a24a
Stefan Malanik <stefanmalanik@gmail.com>
2021-03-27 20:10:53 +0200

    Test

--- R_Trees/R_Trees.cpp
+++ R_Trees/R_Trees.cpp
@@ -9,6 +9,9 @@ int main() {
 
     ShapeFile shp("C:\\Users\\stefa\\Desktop\\ShapefileTest");
     auto no = shp.getNumberOfElements().get();
+    uniform_int_distribution unif(0, no - 1);
+    default_random_engine re;
+    re.seed(time(0));
     for (int i = 0; i < 10; i++)
     {
         
